f15440c85efa2762e1a6b7e013c2acec8e163a12
harshmalviya2003 <harshmalviya343@gmail.com>
2025-05-28 12:38:21 +0530

    #update

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -30,7 +30,7 @@ export function SiteHeader() {
           <Button
             variant="ghost"
             size="icon"
-            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            onClick={() => setTheme(theme === "light" ? "light" : "dark")}
           >
             <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
